956e4135b51fd6e27afac6e476028479fc3a4417
tiyo5151 <s1F102400224@iniad.org>
2024-06-29 06:38:01 +0900

    test修正

--- server/domain/novel/useCase/novelUseCase.ts
+++ server/domain/novel/useCase/novelUseCase.ts
@@ -1,7 +1,13 @@
-import { transaction } from "service/prismaClient";
+import { load } from 'cheerio';
+import * as iconv from 'iconv-lite';
+import { transaction } from 'service/prismaClient';
 
 export const novelUseCase = {
-  scrape:(aozoraUrl:string):Promise<string> => transaction("RepeatableRead",async(tx) => {
-    return aozoraUrl
-  })
+  scrape: (aozoraUrl: string): Promise<string> =>
+    transaction('RepeatableRead', async (tx) => {
+      const html = await fetch(aozoraUrl).then((b) => b.arrayBuffer());
+      const $ = load(iconv.decode(Buffer.from(html), 'Shift_JIS'));
+      const body = $('body');
+      return body.text();
+    }),
 };
