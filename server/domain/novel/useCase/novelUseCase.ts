b8579fb999b54a08d581783dff0722b43f466d5e
tiyo5151 <s1F102400224@iniad.org>
2024-06-29 03:17:15 +0900

    return

---
+++ server/domain/novel/useCase/novelUseCase.ts
@@ -0,0 +1,7 @@
+import { transaction } from "service/prismaClient";
+
+export const novelUseCase = {
+  scrape:(aozoraUrl:string):Promise<string> => transaction("RepeatableRead",async(tx) => {
+    return aozoraUrl
+  })
+};
