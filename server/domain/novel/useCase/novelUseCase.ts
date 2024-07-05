f74227076d5647a466349c4e5fea5be7a3d945ee
tiyo5151 <s1F102400224@iniad.org>
2024-07-05 19:43:24 +0900

    Revert "return"
    This reverts commit b8579fb999b54a08d581783dff0722b43f466d5e.

--- server/domain/novel/useCase/novelUseCase.ts
+++
@@ -1,7 +0,0 @@
-import { transaction } from "service/prismaClient";
-
-export const novelUseCase = {
-  scrape:(aozoraUrl:string):Promise<string> => transaction("RepeatableRead",async(tx) => {
-    return aozoraUrl
-  })
-};
