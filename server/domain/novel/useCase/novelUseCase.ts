31329cfe2e779200e3ecf9edc4f35128afbbd2c1
tiyo5151 <s1F102400224@iniad.org>
2024-07-05 19:41:12 +0900

    Revert "test修正"
    This reverts commit 956e4135b51fd6e27afac6e476028479fc3a4417.

--- server/domain/novel/useCase/novelUseCase.ts
+++ server/domain/novel/useCase/novelUseCase.ts
@@ -1,13 +1,7 @@
-import { load } from 'cheerio';
-import * as iconv from 'iconv-lite';
-import { transaction } from 'service/prismaClient';
+import { transaction } from "service/prismaClient";
 
 export const novelUseCase = {
-  scrape: (aozoraUrl: string): Promise<string> =>
-    transaction('RepeatableRead', async (tx) => {
-      const html = await fetch(aozoraUrl).then((b) => b.arrayBuffer());
-      const $ = load(iconv.decode(Buffer.from(html), 'Shift_JIS'));
-      const body = $('body');
-      return body.text();
-    }),
+  scrape:(aozoraUrl:string):Promise<string> => transaction("RepeatableRead",async(tx) => {
+    return aozoraUrl
+  })
 };
--- server/tests/api/novel.test.ts
+++ server/tests/api/novel.test.ts
@@ -8,7 +8,7 @@ test(GET(noCookieClient.novel), async () => {
 });
 
 test(POST(noCookieClient.novel), async () => {
-  const aozoraUrl = 'https://www.aozora.gr.jp/cards/000148/files/773_14560.html';
+  const aozoraUrl = 'aiu';
   const res = await noCookieClient.novel.$post({ body: { aozoraUrl } });
-  expect(typeof res).toEqual('string');
+  expect(res).toEqual(aozoraUrl);
 });
